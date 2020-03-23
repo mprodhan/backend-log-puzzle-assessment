2250879b44b4212b9add224de2f7a5614a08945a
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-03-23 12:40:11 -0400

    credited work to some major assistsin understanding the structure of the code.

--- logpuzzle.py
+++ logpuzzle.py
@@ -15,6 +15,7 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 """
+__author__ = """ mprodhan, yabamov, malnakhaleh, stackoverflow """
 
 import os
 import re
